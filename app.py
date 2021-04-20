1338e80cdc1c5d094ba318152309e6b8ac4df6c9
Troy Alfelt <troyalfelt@Troys-MacBook-Pro.local>
2021-04-20 15:18:10 -0400

    linked back/front end, added cookies.

--- app.py
+++ app.py
@@ -1,28 +1,84 @@
-from flask import Flask, render_template, request
+##pull test
+##push test
+##push test 2
+from flask import Flask, render_template, request, make_response
 import psycopg2, hashlib, os
 
+
+
 app = Flask(__name__)
-con = psycopg2.connect(database="kwitter", user="akesh201", password="Matlock",
+con = psycopg2.connect(database="kwitter", user="troyalfelt", password="Matlock",
 host ="127.0.0.1", port="5432")
+
 print("hey it actually worked")
 
-@app.route('/', methods = ['GET', 'POST'])
+@app.route('/')
+def home():
+    usr = request.cookies.get('name')
+    print(usr)
+    return render_template('/home.html')
+
+@app.route('/create', methods = ['GET', 'POST'])
 def register():
+    usr = request.cookies.get('name')
     if request.method == 'POST':
         username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        confirm = request.form['confirm']
         cur = con.cursor()
-        cur.execute("""SELECT * FROM users WHERE user_name = %s""", (username,))    #find any accounts with the same username
+        cur.execute("""SELECT * FROM users WHERE user_name = %s""", (username,))
         account = cur.fetchone()
         if account:
             print('Account already exists')
+        elif password == confirm:
+            cur.execute("""INSERT INTO users VALUES (%s, %s, %s)""", (username, password, email))
+            con.commit()
+            print("Success!")
+            res = make_response(render_template('/home.html'))
+            res.set_cookie('name', username, max_age = 60&60*1200)
+            return res
         else:
-            salt = os.urandom(32)   #Generate random salt
-            password = hashlib.sha3_512(request.form['password'].encode('utf-8') + salt).hexdigest()    #Retrieve password, and hash password and salt
-            cur.execute("""INSERT INTO users VALUES (%s, %s, %s)""", (username, password, salt))    #Store username, password, and salt
-            con.commit()    #Commit changes to the database
-            return render_template('/home.html')
+            print("Password does not match confirmation, please try again.")
 
-    return render_template('/index.html')
+
+    return render_template('/create.html')
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    usr = request.cookies.get('name')
+    print(usr)
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        cur = con.cursor()
+        cur.execute("""SELECT * FROM users WHERE user_name = %s AND password = %s""", (username, password))
+        account = cur.fetchone()
+        if account:
+            print(username)
+            res = make_response(render_template('/home.html'))
+            res.set_cookie('name', username, max_age = 60*60*2*600)
+            usr = request.cookies.get('name')
+            print(usr)
+            return res
+
+        else:
+            print("Incorrect username/password")
+
+    return render_template('/login.html')
+
+@app.route('/logout')
+def leave():
+    res = make_response(render_template('home.html'))
+    res.set_cookie('name', '', expires=0)
+    return res
+
+@app.route('/home')
+def main():
+    print(request.cookies.get('name'))
+    return render_template('/home.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug = True)
