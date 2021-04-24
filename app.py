5cdedcbb085ad6aa6d490da0e1571e9d4677f8de
Troy Alfelt <troyalfelt@Troys-MacBook-Pro.local>
2021-04-24 13:11:38 -0400

    added comment table and template to view users posts

--- app.py
+++ app.py
@@ -25,6 +25,16 @@ host ="127.0.0.1", port="5432")
 ##	PRIMARY KEY (post_id),
 ##	FOREIGN KEY(id) REFERENCES users(id)
 ##);
+##CREATE TABLE comments (
+##	comment_id bigserial,
+##	id bigint,
+##	post_id bigint,
+##	user_name varchar(50),
+##	comment varchar(280),
+##	PRIMARY KEY (comment_id),
+##	FOREIGN KEY(id) REFERENCES users(id),
+##	FOREIGN KEY(post_id) REFERENCES posts(post_id)
+##	);
 
 pepper = r'e_XT<tUB%"Gg4F\or57i{^&MAcAaiH@-z|T&y3w8#HTcp~8GcS9K{Y&x?ZC_dxi}*m<T0sr{in\"SDf2\_\6$*{gqe>E2yDZ]}XJ'
 
@@ -49,8 +59,8 @@ def register():
         email_used = cur.fetchone()
         if account:
             print('Account already exists')
-        ##elif not validate_email(email, check_mx=True):
-            ##print('Invalid email address')
+        elif not validate_email(email, check_mx=True):
+            print('Invalid email address')
         elif email_used:
             print('Email address already in use')
 
@@ -64,8 +74,7 @@ def register():
             password = hashlib.sha256((request.form['password'] + salt + pepper).encode('utf-8')).hexdigest()   #Retrieve password, and hash password and salt
             cur.execute("""INSERT INTO users(user_name, password, email, salt) VALUES (%s, %s, %s, %s)""", (username, password, email, salt))    #Store username, password, salt, and email
             con.commit()    #Commit changes to the database
-            print("Success!")
-            res = make_response(redirect('/cookie'))
+            res = make_response(redirect('/home'))
             res.set_cookie('name', username, max_age = 60&60*1200)
             return res
         else:
@@ -103,14 +112,15 @@ def login():
 @app.route('/cookie')
 def set_id():
     username = request.cookies.get('name')
-    print(username)
     res = make_response(redirect('/home'))
     cur = con.cursor()
     cur.execute("""SELECT id FROM users WHERE user_name = %s""", (username,))
-    str_id = str(cur.fetchone())
-    id = str_id.strip("(,)")
+    tupl = cur.fetchone()
+    id = str(tupl[0])
+    print("here it is")
     print(id)
-    res.set_cookie('id', id, max_age = 60*60*120000)
+    print(type(id))
+    res.set_cookie('id', id, max_age = 1200000000)
     return res
 
 
@@ -138,16 +148,51 @@ def pst():
 def main():
     cur = con.cursor()
     cur.execute("""SELECT * FROM posts""")
-    dict = cur.fetchall()
-    print(dict)
-    return render_template('/home.html', dict = dict)
+    posts = cur.fetchall()
+    cur.execute("""SELECT * FROM comments""")
+    comments = cur.fetchall()
+    return render_template('/home.html', posts = posts, comments = comments)
 
-@app.route('/delete/<post>', methods = ['POST'])
-def remove(post):
-    cur = con.cursor()
-    cur.execute("""DELETE FROM posts WHERE post_id = %s""", (post,))
-    con.commit()
+@app.route('/delete/<type>/<id_num>', methods = ['POST'])
+def remove(type, id_num):
+    if request.method == "POST":
+        if type == "pst":
+            cur = con.cursor()
+            cur.execute("""DELETE FROM comments WHERE post_id = %s""", (id_num,))
+            con.commit()
+            cur.execute("""DELETE FROM posts WHERE post_id = %s""", (id_num,))
+            con.commit()
+            print("this deletes a post without comments")
+            return redirect('/home')
+        elif type == "comment":
+            cur = con.cursor()
+            cur.execute("""DELETE FROM comments WHERE comment_id = %s""", (id_num,))
+            con.commit()
+            print("this delets a comment")
+            return redirect('/home')
     return redirect('/home')
+
+@app.route('/comment/<post_id>', methods = ['POST'])
+def comm(post_id):
+    if request.method == "POST":
+        id = int(request.cookies.get('id'))
+        username = request.cookies.get('name')
+        comment = request.form['comment']
+        post = int(post_id)
+        cur = con.cursor()
+        cur.execute("""INSERT INTO comments(id, post_id, user_name, comment) VALUES(%s, %s, %s, %s)""", (id, post, username, comment))
+        con.commit()
+    return redirect('/home')
+
+@app.route('/profile/<user>')
+def render(user):
+    cur = con.cursor()
+    cur.execute("""SELECT * FROM posts WHERE user_name = %s""", (user,))
+    all_posts = cur.fetchall()
+    print(all_posts)
+    return render_template('/profile.html', all_posts = all_posts, user = user)
+
+
 
 if __name__ == '__main__':
     app.run(debug = True)
